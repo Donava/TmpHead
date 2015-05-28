c61c9083b5949b20b5be081c9842ce2916a16b89
donyang <donyangok@gmail.com>
2015-05-28 19:54:57 +0800

    Initial Commit

---
+++ TmpHead/TmpHead.h
@@ -0,0 +1,13 @@
+//
+//  TmpHead.h
+//  TmpHead
+//
+//  Created by Don on 5/28/15.
+//  Copyright (c) 2015 Don. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TmpHead : NSObject
+
+@end
---
+++ TmpHead/TmpHead.m
@@ -0,0 +1,13 @@
+//
+//  TmpHead.m
+//  TmpHead
+//
+//  Created by Don on 5/28/15.
+//  Copyright (c) 2015 Don. All rights reserved.
+//
+
+#import "TmpHead.h"
+
+@implementation TmpHead
+
+@end
